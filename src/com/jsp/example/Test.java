6931bc159ab9f7b8474b852135086f994fae50b4
RaviKiran5791 <ampiravikiran1204@gmail.com>
2024-01-10 11:56:59 +0530

    made change 1 to brance2

--- src/com/jsp/example/Test.java
+++ src/com/jsp/example/Test.java
@@ -4,5 +4,13 @@ public class Test {
 	// change 1 
 	// change 2
 	
-
+	// dev change 1
+	public static void main(String[] args) {
+		System.out.println("Hiii");
+		
+	}
+	public void view()
+	{
+		System.out.println("Branch 2");
+	}
 }
